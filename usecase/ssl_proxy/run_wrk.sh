ab0c0fb4846ead56f247461f340bb4f39d0a847c
yeflin01 <yefeng.lin@arm.com>
2023-03-23 09:38:04 +0800

    update nginx conf and run_wrk.sh

--- usecase/ssl_proxy/run_wrk.sh
+++ usecase/ssl_proxy/run_wrk.sh
@@ -97,7 +97,7 @@ if ! [[ $(command -v "${wrk_binary}") ]]; then
       cd "${DIR}"/../../components
       git clone https://github.com/AmpereTravis/wrk2-aarch64.git
       cd wrk2-aarch64
-      git am "${DIR}"/../../patches/wrk2/0001-wrk2-fd-vpp.patch
+      git am "${DIR}"/../../patches/wrk2/0001_wrk2_fd_vpp.patch
       make all > /dev/null 2>&1
 fi
 
